dded069a6a72311e3c63802ab2c1f5d9933cf2fd
Andreas Åkerberg <zmallwood@proton.me>
2025-05-29 19:13:41 +0200

    Save

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,20 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
 }
 
 module "services_submodule" {
