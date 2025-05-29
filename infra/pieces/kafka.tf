9147301654308bf9853ed08d9b3536b107109339
Andreas Åkerberg <zmallwood@proton.me>
2025-05-30 00:15:43 +0200

    Kafka test

--- infra/pieces/kafka.tf
+++ infra/pieces/kafka.tf
@@ -13,26 +13,111 @@ resource "helm_release" "pieces_kafka" {
         pullSecrets = ["auth-docker"]
       }
 
-      replicaCount = 1
-
-      service = {
-        type            = "LoadBalancer"
-        loadBalancerIP  = "192.168.1.244"  # optional: your LB IP
-        port            = 9092
-        nodePorts       = { client = 32222 } # if you want nodeport as well
+defaultInitContainers = {
+        autoDiscovery = {
+        enabled=true
+        }
       }
+
+      kafka = {
+  kafkaProcessRoles = "controller"
+  kafkaControllerListenerNames = "CONTROLLER"
+  kafkaListeners = "CONTROLLER://:9093"
+      }
+
+      controller = {
+        automountServiceAccountToken = true
+
+        controllerOnly = true
+        #readinessProbe = {
+        #  enabled = false
+        #}
+      }
+      
+        broker = {
+        automountServiceAccountToken = true
+
+      }
+
+      enabled = true
+
+      broker = {
+  replicaCount = 1
+        automountServiceAccountToken = true
+
+        readinessProbe = {
+          enabled = false
+        }
+}
+
+    externalAccess = {
+      enabled = true
+      service = {
+        type   = "NodePort"
+        domain = "localhost"
+      }
+      autoDiscovery = {
+        enabled = true
+      }
+broker = {
+    service = {
+      type       = "NodePort"
+        domain = "localhost"
+      nodePorts  = [32222]  # One for each broker
+      useHostIPs = true
+    }
+
+        readinessProbe = {
+          enabled = false
+        }
+  }
+
+    }
+
+    serviceAccount = {
+      create = true
+    }
+
+    rbac = {
+      create = true
+    }
+
 
       # Allow plaintext listener (for testing/dev)
       allowPlaintextListener = true
 
-      # Configure listeners & advertised listeners
-      kafkaListeners = "PLAINTEXT://:9092,CONTROLLER://:9093"
-      kafkaAdvertisedListeners = "PLAINTEXT://localhost:9092"
-      kafkaListenerSecurityProtocolMap = "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT"
-      kafkaProcessRoles = "broker,controller"
-      kafkaControllerListenerNames = "CONTROLLER"
-      kafkaControllerQuorumVoters = "1@localhost:9093"
-      kafkaNodeId = 1
+      listeners = {
+  #client:
+  #  protocol: 'PLAINTEXT'
+  controller= {
+    protocol= "PLAINTEXT"
+        }
+  #external:
+  #  protocol: 'PLAINTEXT'
+        #advertisedListeners: "PLAINTEXT://localhost:32222"
+  advertisedListeners: "PLAINTEXT://localhost:32222"
+      }
+
+      #listeners = {
+      #  client = {
+      #    protocol = "PLAINTEXT"
+      #  }
+      #}
+
+      #auth = {
+      #  clientProtocol = "plaintext"
+      #  interBrokerProtocol = "plaintext"
+      #}
+
+      ## Configure listeners & advertised listeners
+      #kafkaListeners = "PLAINTEXT://:9092,CONTROLLER://:9093"
+      ##kafkaAdvertisedListeners = "PLAINTEXT://localhost:9092"
+      #kafkaAdvertisedListeners = "PLAINTEXT://192.168.1.244:32222"
+      #kafkaListenerSecurityProtocolMap = "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT"
+      #kafkaProcessRoles = "broker,controller"
+      #kafkaControllerListenerNames = "CONTROLLER"
+      #kafkaControllerQuorumVoters = "1@localhost:9093"
+      #kafkaNodeId = 1
 
       persistence = {
         enabled = true
