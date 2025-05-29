ee9d9fdecc383afaae5a8a66b70b93e93f32b9e9
Andreas Åkerberg <zmallwood@proton.me>
2025-05-29 12:16:58 +0200

    Save

--- backend/services/main-api/src/index.ts
+++ backend/services/main-api/src/index.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+// Enable CORS for all origins
+app.use(cors());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from TypeScript backend!");
+  //res.send("Hello from TypeScript backend!");
+  //const data = {
+  //  commands: ["Clear;0;150;255;"]
+  //};
+
+  const data = {
+    commands: ["DrawImage;DefaultSceneBackground;0.0;0.0;1.0;1.0;false;"]
+  };
+
+  res.json(data); // Respond with JSON
+//  res.send({\"commands\": "
+//              "[\"DrawImage;GroundGrass;0.0;0.0;0.2;0.2;false;\"]})
 });
 
 app.listen(port, () => {
